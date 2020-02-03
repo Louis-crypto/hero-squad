41d88e858cba64095c0adf324e25e3ac3b2bd430
Louis-crypto <kinglouis745@gmail.com>
2020-02-04 02:27:28 +0300

    created a taste for instantiating hero class

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -2,10 +2,20 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class HeroTest {
+    public Hero myHero(){
+        return new Hero("Dracula",21,"speed","sunlight");
+    }
+
+    @Test
+    public void newHeroGetsCorrectlyInstantiated_true() {
+        Hero hero = myHero();
+        assertEquals(true, hero instanceof Hero);
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -14,4 +24,5 @@ public class HeroTest {
     @After
     public void tearDown() throws Exception {
     }
+
 }
