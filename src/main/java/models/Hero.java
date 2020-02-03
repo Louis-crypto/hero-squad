58ad0bd16ff8727152c64ba8fa2433a41739a3f9
Louis-crypto <kinglouis745@gmail.com>
2020-02-03 17:36:39 +0300

    create an array list for Heroes class

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -7,12 +7,16 @@ public class Hero {
     private int age;
     private String superPower;
     private String weakness;
+    private int id;
+    private static  ArrayList<Hero> listOfHeroes = new ArrayList<>();
 
     public Hero(String name, int age, String superPower, String weakness){
         this.name = name;
         this.age = age;
         this.superPower = superPower;
         this.weakness = weakness;
+
     }
+
 }
 
