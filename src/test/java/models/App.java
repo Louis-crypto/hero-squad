04e8fa01316f6e79d1131626a404991e6f7c75e6
Louis-crypto <kinglouis745@gmail.com>
2020-02-04 05:12:23 +0300

    add a form file for creating new Hero

--- src/test/java/models/App.java
+++ src/test/java/models/App.java
@@ -6,9 +6,15 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
+import models.Hero;
 
 public class App {
     public static void main(String[] args){
     staticFileLocation("/public");
+
+    get("/", (req, res) ->{
+        Map<String, Object> model = new HashMap<>();
+        return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
